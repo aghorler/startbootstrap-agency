d22dca84e57e422991d863c21d156bbb4253cc72
Aaron Horler <aghorler@users.noreply.github.com>
2017-10-21 18:23:21 +1100

    Rotate header image

--- js/custom.js
+++ js/custom.js
@@ -11,5 +11,26 @@ function resize(){
   }
 }
 
-document.addEventListener('DOMContentLoaded', resize);
-window.addEventListener('resize', resize);
+function rotateImage(){
+  var total = header.dataset.total;
+
+  if(position == total){
+    position = 1;
+  }
+  else{
+    position++;
+  }
+
+  header.style.backgroundImage = 'url("./img/header/header0' + position + '.jpg")';
+}
+
+if(document.getElementById("header") !== null){
+  var header = document.getElementById("header");
+  var style = window.getComputedStyle(header);
+  var image = style.getPropertyValue('background-image');
+  var position = image.charAt(image.length-7);
+
+  document.addEventListener('DOMContentLoaded', resize);
+  window.addEventListener('resize', resize);
+  setInterval(rotateImage, 4000);
+}
